05759c7ccb8f707b1280a97bdf72a8e6aad1d6a2
Lord-Fifth <aditya.suresh1504@gmail.com>
2019-07-12 14:49:23 +0530

    try-catch

--- src/main/java/jsonParserMvn/jsonParserMvn/ParseJson.java
+++ src/main/java/jsonParserMvn/jsonParserMvn/ParseJson.java
@@ -1,9 +1,35 @@
 package jsonParserMvn.jsonParserMvn;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class ParseJson 
 {
     public static void main( String[] args )
     {
-
+    	JSONParser parser = new JSONParser();
+    	
+    	try
+    	{
+    		Object obj = parser.parse(new FileReader("plans.json"));
+    	}
+    	
+    	catch(FileNotFoundException e) {
+    		e.printStackTrace();	
+    	}
+    	catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    	catch(ParseException e) {
+    		e.printStackTrace();
+    	}
+    	catch(Exception e) {
+    		e.printStackTrace();
+    	}
+    	
     }
 }
