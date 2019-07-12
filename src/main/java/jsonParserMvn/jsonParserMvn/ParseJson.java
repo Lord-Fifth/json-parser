0d8c145e77167b6492400102eacd1909e8b9b47b
Lord-Fifth <aditya.suresh1504@gmail.com>
2019-07-12 18:01:56 +0530

    Errored code

--- src/main/java/jsonParserMvn/jsonParserMvn/ParseJson.java
+++ src/main/java/jsonParserMvn/jsonParserMvn/ParseJson.java
@@ -3,7 +3,10 @@ package jsonParserMvn.jsonParserMvn;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
@@ -12,11 +15,34 @@ public class ParseJson
     public static void main( String[] args )
     {
     	JSONParser parser = new JSONParser();
-    	
+    		
     	try
     	{
     		Object obj = parser.parse(new FileReader("plans.json"));
-    	}
+    		JSONObject jsonObject = (JSONObject) obj;
+    		
+    		String pid = (String) jsonObject.get("planId");
+    		String pname = (String) jsonObject.get("planName");
+    		String price = (String) jsonObject.get("planPrice");
+    		String pdesc = (String) jsonObject.get("planDesc");
+    		String pstat = (String) jsonObject.get("planStatus");
+    		
+    		//Loop array
+    		JSONArray smsArray = (JSONArray) jsonObject.get("smsPlans");
+    		JSONArray dataArray = (JSONArray) jsonObject.get("dataPlans");
+    		Iterator<String> it1 = smsArray.iterator();
+    		Iterator<String> it2 = dataArray.iterator();
+    		
+    		while(it1.hasNext()) {
+    			System.out.println("smsPlans : " + it1.next());
+    			System.out.println("planId : " + pid + "\nplanName : " + pname + "\nplanPrice : " + price + "\nplanDesc : " + pdesc + "\nplanStatus : " + pstat);
+    		}
+    		
+    		while(it2.hasNext()) {
+    			System.out.println("dataPlans : " + it2.next());
+    			System.out.println("planId : " + pid + "\nplanName : " + pname + "\nplanPrice : " + price + "\nplanDesc : " + pdesc + "\nplanStatus : " + pstat);
+    		}
+    	}	
     	
     	catch(FileNotFoundException e) {
     		e.printStackTrace();	
